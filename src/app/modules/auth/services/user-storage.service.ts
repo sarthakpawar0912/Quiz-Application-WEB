b55b8b39eb47e0a9f7b9db3ddfd620e9e329c061
sarthakpawar0912 <pawarsr06@gmail.com>
2025-02-27 13:43:25 +0530

    Updated features and fixed issues

--- src/app/modules/auth/services/user-storage.service.ts
+++ src/app/modules/auth/services/user-storage.service.ts
@@ -2,40 +2,51 @@ import { Injectable } from '@angular/core';
 
 const USER = 'q_user';
 
+
 @Injectable({
   providedIn: 'root'
 })
+
 export class UserStorageService {
 
+
   constructor() {}
+
 
   saveUser(user: any): void {
     window.localStorage.removeItem(USER);
     window.localStorage.setItem(USER, JSON.stringify(user));
   }
 
+
   getUser(): any {
     const user = localStorage.getItem(USER);
     return user ? JSON.parse(user) : null;
   }
+
   
   getUserId(): string {
     return this.getUser()?.id || '';
   }
 
+
   getUserRole(): string {
     return this.getUser()?.role || '';
   }
+
 
   isAdminLoggedIn(): boolean {
     return this.getUserRole() === 'ADMIN';
   }
 
+
   isUserLoggedIn(): boolean {
     return this.getUserRole() === 'USER';
   }
 
+
   signOut(): void {
     window.localStorage.removeItem(USER);
   }
+
 }
