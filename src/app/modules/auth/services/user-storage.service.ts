ea3e913325a432bf0ece3e8723a1bf43641be726
sarthakpawar0912 <pawarsr06@gmail.com>
2025-03-03 21:44:02 +0530

    made some changes in User Storrage Services

--- src/app/modules/auth/services/user-storage.service.ts
+++ src/app/modules/auth/services/user-storage.service.ts
@@ -8,42 +8,33 @@ const USER = 'q_user';
 })
 
 export class UserStorageService {
-
-
   constructor() {}
-
 
   saveUser(user: any): void {
     window.localStorage.removeItem(USER);
     window.localStorage.setItem(USER, JSON.stringify(user));
   }
 
-
   getUser(): any {
     const user = localStorage.getItem(USER);
     return user ? JSON.parse(user) : null;
   }
-
   
   getUserId(): string {
     return this.getUser()?.id || '';
   }
 
-
   getUserRole(): string {
     return this.getUser()?.role || '';
   }
-
 
   isAdminLoggedIn(): boolean {
     return this.getUserRole() === 'ADMIN';
   }
 
-
   isUserLoggedIn(): boolean {
     return this.getUserRole() === 'USER';
   }
-
 
   signOut(): void {
     window.localStorage.removeItem(USER);
