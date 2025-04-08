a336a41b438e4c395af5af93c2bf84e8f299e4e1
sarthakpawar0912 <pawarsr06@gmail.com>
2025-04-08 23:55:19 +0530

    made some chnages

--- src/app/modules/user/user-routing.module.ts
+++ src/app/modules/user/user-routing.module.ts
@@ -3,14 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { TakeTestComponent } from './components/take-test/take-test.component';
 import { ViewMyTestResultsComponent } from './components/view-my-test-results/view-my-test-results.component';
-
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' }, // Redirect base user path to dashboard
   { path: 'dashboard', component: DashboardComponent },
   { path: 'take-test/:id', component: TakeTestComponent },
   { path: 'view-my-test-results', component: ViewMyTestResultsComponent }
 ];
-
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
